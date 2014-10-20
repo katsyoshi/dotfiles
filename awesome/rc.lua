2ab5f0fec8b42dc114abd3b380ecb1c2991095da
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2014-10-20 09:45:21 +0900

    change lunch volumeicon at startup

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -42,11 +42,15 @@ editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
 -- browser
-browser = "google-chrome-stable"
+browser = "firefox-bin"
 mikutter = "mikutter"
 emacs = "emacs"
 gimp = "gimp"
 sleep = "xscreensaver-command --lock"
+none_display = "sh /home/katsu/.screenlayout/none.sh"
+play_display = "sh /home/katsu/.screenlayout/play.sh"
+work_display = "sh /home/katsu/.screenlayout/work.sh"
+laptop_display = "sh /home/katsu/.screenlayout/laptop.sh"
 
 -- Default modkey.
 -- Usually, Mod4 is the key with a logo between Control and Alt.
@@ -230,20 +234,24 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey, "Control" }, "r", awesome.restart),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit),
 
-    awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)    end),
-    awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05)    end),
-    awful.key({ modkey, "Shift"   }, "h",     function () awful.tag.incnmaster( 1)      end),
-    awful.key({ modkey, "Shift"   }, "l",     function () awful.tag.incnmaster(-1)      end),
-    awful.key({ modkey, "Control" }, "h",     function () awful.tag.incncol( 1)         end),
-    awful.key({ modkey, "Control" }, "l",     function () awful.tag.incncol(-1)         end),
-    awful.key({ modkey,           }, "space", function () awful.layout.inc(layouts,  1) end),
-    awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(layouts, -1) end),
-    awful.key({ modkey, "Shift"   }, "b",     function () awful.util.spawn(browser)     end),
-    awful.key({ modkey, "Shift"   }, "t",     function () awful.util.spawn(twitter)     end),
-    awful.key({ modkey, "Shift"   }, "e",     function () awful.util.spawn(editor)      end),
-    awful.key({ modkey, "Shift"   }, "l",     function () awful.util.spawn(sleep)       end),
+    awful.key({ modkey,           }, "l",    function () awful.tag.incmwfact( 0.05)    end),
+    awful.key({ modkey,           }, "h",    function () awful.tag.incmwfact(-0.05)    end),
+    awful.key({ modkey, "Shift"   }, "h",    function () awful.tag.incnmaster( 1)      end),
+    awful.key({ modkey, "Shift"   }, "l",    function () awful.tag.incnmaster(-1)      end),
+    awful.key({ modkey, "Control" }, "h",    function () awful.tag.incncol( 1)         end),
+    awful.key({ modkey, "Control" }, "l",    function () awful.tag.incncol(-1)         end),
+    awful.key({ modkey,           }, "space",function () awful.layout.inc(layouts,  1) end),
+    awful.key({ modkey, "Shift"   }, "space",function () awful.layout.inc(layouts, -1) end),
+    awful.key({ modkey, "Shift"   }, "b",    function () awful.util.spawn(browser)     end),
+    awful.key({ modkey, "Shift"   }, "t",    function () awful.util.spawn(twitter)     end),
+    awful.key({ modkey, "Shift"   }, "e",    function () awful.util.spawn(editor)      end),
+    awful.key({ modkey, "Shift"   }, "l",    function () awful.util.spawn(sleep)       end),
+    awful.key({ modkey, "Control" }, "w",    function () awful.util.spawn(work_display) end),
+    awful.key({ modkey, "Control" }, "g",    function () awful.util.spawn(laptop_display) end),
+    awful.key({ modkey, "Control" }, "n",    function () awful.util.spawn(none_display) end),
+    awful.key({ modkey, "Control" }, "p",    function () awful.util.spawn(play_display) end),
+    awful.key({ modkey, "Control" }, "n",    awful.client.restore),
 
-    awful.key({ modkey, "Control" }, "n", awful.client.restore),
 
     -- Prompt
     awful.key({ modkey },            "r",     function () mypromptbox[mouse.screen]:run() end),
@@ -382,6 +390,5 @@ client.add_signal("unfocus", function(c) c.border_color = beautiful.border_norma
 -- awful.util.spawn_with_shell("")
 awful.util.spawn_with_shell("xfce4-power-manager") -- Battery monitor, etc.
 awful.util.spawn_with_shell("setxkbmap -option ctrl:nocaps") -- change capslock to ctrl key
-awful.util.spawn_with_shell("volumeicon")
 awful.util.spawn_with_shell("xscreensaver")
 awful.util.spawn_with_shell("ibus-daemon -x -d")
