5550417cbdfccef9fb3d721287f60c20588afc86
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2015-03-10 09:45:42 +0900

    change window move key

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -204,8 +204,8 @@ root.buttons(awful.util.table.join(
 
 -- {{{ Key bindings
 globalkeys = awful.util.table.join(
-    awful.key({ "Control",           }, "Left",   awful.tag.viewprev       ),
-    awful.key({ "Control",           }, "Right",  awful.tag.viewnext       ),
+    awful.key({ modkey,           }, "Left",   awful.tag.viewprev       ),
+    awful.key({ modkey,           }, "Right",  awful.tag.viewnext       ),
     awful.key({ modkey,           }, "Escape", awful.tag.history.restore),
 
     awful.key({ modkey,           }, "j",
