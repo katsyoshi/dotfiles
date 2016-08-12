fc9baa85ace48dd2c084f2de90bebdc932b879f1
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2016-08-12 12:32:59 +0900

    use proc

--- h2o/mruby/fallback_404.rb
+++ h2o/mruby/fallback_404.rb
@@ -1,4 +1,3 @@
-r = H2O::Request.new
-r.status = 404
-r.reason = "File Not Found"
-r.send_file("/opt/website/top/error/404.html")
+Proc.new do |env|
+  [404, {'content-type' => 'text/plain'}, File.open('/opt/website/top/error/404.html')]
+end
