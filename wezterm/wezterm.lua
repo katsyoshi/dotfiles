24fb90913a760f14627463d71367d61142b1baaa
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2022-03-17 10:51:00 +0900

    set tab color and font configuration and show date time

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -32,6 +32,56 @@ end
 base_dir = wezterm.home_dir .. "/.local/share/wezterm/"
 socket = base_dir .. "wezterm.socket"
 
+local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+local SOLID_RIGHT_ARROW = utf8.char(0xe0b0)
+
+wezterm.on("update-right-status", function(window, pane)
+  local date = wezterm.strftime("%Y/%m/%d(%a) %H:%M ");
+
+  local bat = ""
+
+  for _, b in ipairs(wezterm.battery_info()) do
+    bat = "🔋 " .. string.format("%.0f%%", b.state_of_charge * 100)
+  end
+
+  window:set_right_status(wezterm.format({
+    { Background = { Color = "#2b2042", }, },
+    { Foreground = { Color = "#808080", }, },
+    { Text = bat .. "   "..date },
+  }));
+end)
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+  local edge_background = "#0b0022"
+  local background = "#1b1032"
+  local foreground = "#808080"
+
+  if tab.is_active then
+    background = "#2b2042"
+    foreground = "#c0c0c0"
+  elseif hover then
+    background = "#3b3052"
+    foreground = "#909090"
+  end
+
+  local edge_foreground = background
+
+  -- ensure that the titles fit in the available space,
+  -- and that we have room for the edges.
+  local title = wezterm.truncate_right(tab.active_pane.title, max_width-2)
+
+  return {
+    { Background = { Color = edge_background }, },
+    { Foreground = { Color = edge_foreground }, },
+    { Text = SOLID_LEFT_ARROW },
+    { Background = { Color = background, }, },
+    { Foreground = { Color = foreground, }, },
+    { Text = title},
+    { Background = { Color = edge_background, }, },
+    { Foreground = { Color = edge_foreground, }, },
+    { Text = SOLID_RIGHT_ARROW, },
+  }
+end)
 return {
   font = wezterm.font"Noto Mono for Powerline",
   use_ime = true,
