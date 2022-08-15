14f9c00f28e3fdc2e00012125da3f4c570d566db
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2022-08-15 11:14:12 +0900

    fix indent for lua-mode

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -45,21 +45,30 @@ local assigned_keys = {
 
 for i = 0, 9 do
    local key_string = tostring(i)
-   table.insert(assigned_keys,{
-     key = key_string,
-     mods = "LEADER|ALT",
-     action = act{ ActivateTab = i },
-   })
-   table.insert(assigned_keys,{
-     key = key_string,
-     mods = "LEADER|CTRL",
-     action = act{ MoveTab = i },
-   })
-   table.insert(assigned_keys,{
-     key = key_string,
-     mods = "LEADER",
-     action = act{ ActivatePaneByIndex = i },
-   })
+   table.insert(
+      assigned_keys,
+      {
+         key = key_string,
+         mods = "LEADER|ALT",
+         action = act { ActivateTab = i },
+      }
+   )
+   table.insert(
+      assigned_keys,
+      {
+         key = key_string,
+         mods = "LEADER|CTRL",
+         action = act{  MoveTab = i },
+      }
+   )
+   table.insert(
+      assigned_keys,
+      {
+         key = key_string,
+         mods = "LEADER",
+         action = act{ ActivatePaneByIndex = i },
+      }
+   )
 end
 
 local base_dir = wezterm.home_dir .. "/.local/share/wezterm/"
@@ -68,78 +77,85 @@ local socket = base_dir .. "wezterm.socket"
 local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
 local SOLID_RIGHT_ARROW = utf8.char(0xe0b0)
 
-wezterm.on("update-right-status", function(window, _)
-  local date = wezterm.strftime("%Y/%m/%d(%a) %H:%M ");
+wezterm.on(
+   "update-right-status",
+   function(window, _)
+      local date = wezterm.strftime("%Y/%m/%d(%a) %H:%M ");
 
-  local bat = ""
+      local bat = ""
+      for _, b in ipairs(wezterm.battery_info()) do
+         bat = "🔋 " .. string.format("%.0f%%", b.state_of_charge * 100)
+      end
 
-  for _, b in ipairs(wezterm.battery_info()) do
-    bat = "🔋 " .. string.format("%.0f%%", b.state_of_charge * 100)
-  end
-
-  window:set_right_status(wezterm.format({
-    { Background = { Color = "#2b2042", }, },
-    { Foreground = { Color = "#808080", }, },
-    { Text = bat .. "   "..date },
-  }));
+      window:set_right_status(
+         wezterm.format(
+            {
+               { Background = { Color = "#2b2042", }, },
+               { Foreground = { Color = "#808080", }, },
+               { Text = bat .. "   "..date },
+            }
+         )
+      );
 end)
 
-wezterm.on("format-tab-title", function(tab, _, _, _, hover, max_width)
-  local edge_background = "#0b0022"
-  local background = "#1b1032"
-  local foreground = "#808080"
+wezterm.on(
+   "format-tab-title",
+   function(tab, _, _, _, hover, max_width)
+      local edge_background = "#0b0022"
+      local background = "#1b1032"
+      local foreground = "#808080"
 
-  if tab.is_active then
-    background = "#2b2042"
-    foreground = "#c0c0c0"
-  elseif hover then
-    background = "#3b3052"
-    foreground = "#909090"
-  end
+      if tab.is_active then
+         background = "#2b2042"
+         foreground = "#c0c0c0"
+      elseif hover then
+         background = "#3b3052"
+         foreground = "#909090"
+      end
 
-  local edge_foreground = background
+      local edge_foreground = background
 
-  -- ensure that the titles fit in the available space,
-  -- and that we have room for the edges.
-  local title = wezterm.truncate_right(tab.active_pane.title, max_width-2)
+      -- ensure that the titles fit in the available space,
+      -- and that we have room for the edges.
+      local title = wezterm.truncate_right(tab.active_pane.title, max_width-2)
 
-  return {
-    { Background = { Color = edge_background }, },
-    { Foreground = { Color = edge_foreground }, },
-    { Text = SOLID_LEFT_ARROW },
-    { Background = { Color = background, }, },
-    { Foreground = { Color = foreground, }, },
-    { Text = title },
-    { Background = { Color = edge_background, }, },
-    { Foreground = { Color = edge_foreground, }, },
-    { Text = SOLID_RIGHT_ARROW, },
-  }
+      return {
+         { Background = { Color = edge_background }, },
+         { Foreground = { Color = edge_foreground }, },
+         { Text = SOLID_LEFT_ARROW },
+         { Background = { Color = background, }, },
+         { Foreground = { Color = foreground, }, },
+         { Text = title },
+         { Background = { Color = edge_background, }, },
+         { Foreground = { Color = edge_foreground, }, },
+         { Text = SOLID_RIGHT_ARROW, },
+      }
 end)
 
 return {
-  adjust_window_size_when_changing_font_size = false,
-  color_scheme = "Dracula",
-  default_gui_startup_args = { "connect", "wezterm" },
-  disable_default_key_bindings = true,
-  font = wezterm.font "Noto Mono for Powerline",
-  font_size = 10.0,
-  keys = assigned_keys,
-  key_tables = {
-     copy_mode = cursols,
-  },
-  leader = { key="t", mods="CTRL", timeout_milliseconds=1000 },
-  tab_bar_at_bottom = true,
-  use_ime = true,
+   adjust_window_size_when_changing_font_size = false,
+   color_scheme = "Dracula",
+   default_gui_startup_args = { "connect", "wezterm" },
+   disable_default_key_bindings = true,
+   font = wezterm.font "Noto Mono for Powerline",
+   font_size = 10.0,
+   keys = assigned_keys,
+   key_tables = {
+      copy_mode = cursols,
+   },
+   leader = { key="t", mods="CTRL", timeout_milliseconds=1000 },
+   tab_bar_at_bottom = true,
+   use_ime = true,
 
-  unix_domains = {
-    {
-      name = "wezterm",
-      socket_path = socket,
-    },
-  },
-  daemon_options = {
-    stdout = base_dir .. "stdout",
-    stderr = base_dir .. "stderr",
-    pid_file = base_dir .. "pid",
-  }
+   unix_domains = {
+      {
+         name = "wezterm",
+         socket_path = socket,
+      },
+   },
+   daemon_options = {
+      stdout = base_dir .. "stdout",
+      stderr = base_dir .. "stderr",
+      pid_file = base_dir .. "pid",
+   }
 }
